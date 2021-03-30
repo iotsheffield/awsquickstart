78adbdab67333418968f2a1cd2ba82554844f12c
David R. White <david.r.white@sheffield.ac.uk>
2021-03-30 12:18:06 +0100

    subscribe wip

--- subscribe.py
+++ subscribe.py
@@ -1,6 +1,7 @@
-from awscrt import io, mqtt, auth, http
+from awscrt import io, mqtt
 from awsiot import mqtt_connection_builder
-
+import os
+import threading
 
 
 def on_message_received(topic, payload, dup, qos, retain, **kwargs):
@@ -10,21 +11,26 @@ def on_message_received(topic, payload, dup, qos, retain, **kwargs):
 event_loop_group = io.EventLoopGroup(1)
 host_resolver = io.DefaultHostResolver(event_loop_group)
 client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
-mqtt_connection = mqtt_connection_builder.mtls_from_path(endpoint="ENDPOINT", cert_filepath="cert.pem.crt",
+
+mqtt_connection = mqtt_connection_builder.mtls_from_path(endpoint=os.getenv('IOT_ENDPOINT'),
+                                                         cert_filepath="cert.pem.crt",
                                                          pri_key_filepath="private.pem.key",
                                                          client_bootstrap=client_bootstrap,
                                                          ca_filepath="AmazonRootCA1.pem",
-                                                         client_id="simplething",
-                                                         clean_session=False,
-                                                         keep_alive_secs=6)
+                                                         client_id="simplething")
 
 mqtt_connection.connect().result()
+
 subscribe_future, packet_id = mqtt_connection.subscribe(
-    topic=args.topic,
+    topic="test/testing",
     qos=mqtt.QoS.AT_LEAST_ONCE,
     callback=on_message_received)
+
 subscribe_result = subscribe_future.result()
-while True:
-    pass
+
+threading.Event().wait();
+
+# while True:
+#     pass
 
 mqtt_connection.disconnect().result()
